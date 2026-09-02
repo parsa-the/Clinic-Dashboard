d6daf3a2a78a5bf80d38cb75fa538d4f465850b5
Parsa Parvizi <parviziparsa542@gmail.com>
2026-09-02 05:32:07 +0500

    blocked slots now save to local stroage

--- src/mocks/availability.ts
+++ src/mocks/availability.ts
@@ -1,10 +1,17 @@
 import type { ConsultantAvailability } from "@/types";
 
-export let consultantAvailability: ConsultantAvailability = {
-  consultantId: "2",
-  blockedDates: [],
-};
+const STORAGE_KEY = "consultant-availability";
+
+export let consultantAvailability: ConsultantAvailability =
+  JSON.parse(
+    localStorage.getItem(STORAGE_KEY) ||
+      JSON.stringify({
+        consultantId: "2",
+        blockedDates: [],
+      }),
+  );
 
 export const setConsultantAvailability = (value: ConsultantAvailability) => {
   consultantAvailability = value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(value));
 };
